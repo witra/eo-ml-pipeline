c1bb7a5c89e708785b4679b2cfc52e56deeaadce
witra <tahjudil.witra@gmail.com>
2026-08-22 17:22:02 +0200

    optimize kwags and saving

--- src/eo_pipeline/discovery/stac.py
+++ src/eo_pipeline/discovery/stac.py
@@ -1,25 +1,44 @@
 import logging
+import os
+
+import geopandas as gpd
 import pystac_client
+
+from eo_pipeline.utils.geom import bbox_to_epsg
 
 logger = logging.getLogger(__name__)
 
-def search_s2(bbox, 
-              datetime, 
-              catalog="https://planetarycomputer.microsoft.com/api/stac/v1",
+def search_s2(catalog="https://planetarycomputer.microsoft.com/api/stac/v1",
               collection="sentinel-2-l2a",
-              max_cloud_cover=20, 
-              max_item=10,
-              kwargs_search=None):
-
-    if not kwargs_search:
-        kwargs_search = dict(
-            bbox=bbox,
-            datetime=datetime,
-            query={"eo:cloud_cover": {"lte": max_cloud_cover}},
-            max_items=max_item
-            )       
+              save_dir=None, 
+              basename=None,
+              **kwargs):
+    """
+    datetime: range from/to "yyyy-mm-dd/yyyy-mm-dd"
+    """
+    kwargs_search = {
+        "bbox": kwargs['bbox'],
+        "datetime": kwargs['datetime'],
+        "query": kwargs['query'],
+        "max_items": kwargs['max_items'],
+        }
+    proj_epsg =  bbox_to_epsg(*kwargs['bbox'])       
     catalog_pystac = pystac_client.Client.open(catalog)
     search = catalog_pystac.search(collections=[collection], **kwargs_search)
     items = search.item_collection()
-    logger.info(f"number of S2 scenes found of {bbox}: {len(items)}")
+    logger.info(f"number of S2 scenes found of {kwargs['bbox']}: {len(items)}")
+    if len(items) > 0 and save_dir and basename:
+        if save_dir.startswith("s3://"):
+                pass # TODO save to s3 bucket
+        else: 
+            child_dir = basename.rsplit("/", 1)[0] if "/" in basename else ""
+            os.makedirs(f"{save_dir}/{child_dir}", exist_ok=True)
+        df = gpd.GeoDataFrame.from_features(items.to_dict(), crs=f"epsg:{proj_epsg}")
+        df.to_csv(f'{save_dir}/{basename}.csv')
     return items
+
+def search_items(platform, **kwargs):
+    if 'S2' in platform:
+        return search_s2(**kwargs)
+    else: 
+        logger.warning(f'The {platform} is not available yet. Please check the available platforms')
