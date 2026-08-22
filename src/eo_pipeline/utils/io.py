0b91a3bca26a53b0256d25a096201f4f16219fdf
witra <tahjudil.witra@gmail.com>
2026-08-22 17:16:19 +0200

    add io tool

---
+++ src/eo_pipeline/utils/io.py
@@ -0,0 +1,15 @@
+import logging
+
+logger = logging.getLogger(__name__)
+
+def save_xarray(ds, save_dir, filename, save_format='zarr', **kwargs):
+    if save_format == 'zarr':
+        path = f'{save_dir}/{filename}.zarr'
+        ds.to_zarr(path, mode="w", consolidated=False, zarr_format=3)
+        return path
+    elif save_format == 'tif':
+        path = f'{save_dir}/{filename}.tif'
+        ds.rio.to_raster(path, driver="COG", compress="ZSTD", blocksize=512, overview_resampling="nearest")
+        return path
+    else:
+        logger.info(f'the save_format {save_format} is not available yet')
