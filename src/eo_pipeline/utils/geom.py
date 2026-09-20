f07aac2c016095f25e8a6f0afbd399628b51aef4
witra <tahjudil.witra@gmail.com>
2026-09-20 12:46:37 +0200

    add docs

--- src/eo_pipeline/utils/geom.py
+++ src/eo_pipeline/utils/geom.py
@@ -9,6 +9,22 @@ from pyproj.database import query_utm_crs_info
 logger = logging.getLogger(__name__)
 
 def get_bbox(lon, lat, half_size_m):
+    """Create a bounding box around a geographic point.
+
+    Parameters
+    ----------
+    lon : float
+        Longitude of the center point.
+    lat : float
+        Latitude of the center point.
+    half_size_m : float
+        Half-size of the bounding box in meters.
+
+    Returns
+    -------
+    list of float
+        Bounding box as ``[min_lon, min_lat, max_lon, max_lat]``.
+    """
     easting, northing, zone_number, zone_letter = utm.from_latlon(lat,lon)
     xmin = easting - half_size_m
     xmax = easting + half_size_m
@@ -19,6 +35,19 @@ def get_bbox(lon, lat, half_size_m):
     return [min_lon.item(), min_lat.item(), max_lon.item(), max_lat.item()]
 
 def bbox_to_epsg(min_lon, min_lat, max_lon, max_lat):
+    """Get the UTM EPSG code for a bounding box.
+
+    Parameters
+    ----------
+    min_lon, min_lat, max_lon, max_lat : float
+        Bounding box coordinates in WGS 84.
+
+    Returns
+    -------
+    str or None
+        EPSG code of the applicable WGS 84 UTM CRS, or ``None`` if no
+        matching CRS is found.
+    """
     crs_list = query_utm_crs_info(
         datum_name="WGS 84",
         area_of_interest=AreaOfInterest(
@@ -31,6 +60,18 @@ def bbox_to_epsg(min_lon, min_lat, max_lon, max_lat):
     return crs_list[0].code if crs_list else None
 
 def get_bbox_from_tif(path):
+    """Get a WGS 84 bounding box from a raster file.
+
+    Parameters
+    ----------
+    path : str or Path
+        Path to the raster file.
+
+    Returns
+    -------
+    list of float
+        Bounding box as ``[min_lon, min_lat, max_lon, max_lat]``.
+    """
     src = rio.open(path)
     bbox = src.bounds
     crs = src.crs
--- src/eo_pipeline/utils/io.py
+++ src/eo_pipeline/utils/io.py
@@ -1,8 +1,30 @@
 import logging
+import xarray as xr
+import rioxarray as rio
 
 logger = logging.getLogger(__name__)
 
 def save_xarray(ds, save_dir, filename, save_format='zarr', **kwargs):
+    """Save an xarray dataset as Zarr or Cloud Optimized GeoTIFF.
+
+    Parameters
+    ----------
+    ds : xarray.Dataset
+        Dataset to save.
+    save_dir : str
+        Directory where the dataset is saved.
+    filename : str
+        Output filename without the file extension.
+    save_format : {"zarr", "tif"}, default="zarr"
+        Output format.
+    **kwargs
+        Additional options. For Zarr, ``encoding`` can be provided.
+
+    Returns
+    -------
+    str
+        Path to the saved dataset, or ``None`` if the format is unsupported.
+    """
     if save_format == 'zarr':
         path = f'{save_dir}/{filename}.zarr'
         encoding = kwargs.get("encoding", None)
